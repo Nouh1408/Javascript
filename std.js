5df062b6155954283557d4367eb12c8415b5ac8f
Ahmed Ihab <ahmedinouh@gmail.com>
2024-02-17 00:12:25 +0200

    just studying

--- std.js
+++ std.js
@@ -1,3 +1,21 @@
-var mystring = "Hello ";
-var sec = mystring+"World";
-console.log(sec);
+/* function freeshipping(){
+    console.log("Freeshipping");
+}
+function nofree(){
+    console.log("No ff");
+}
+
+
+var price = 12.99;
+var discount = 10;
+var total = price -(price *(discount/100));
+if(total > 25){
+    freeshipping();
+}
+else{
+    nofree();
+} */
+
+var mystr = "jello";
+mystr[0] = "H";
+console.log(mystr);
