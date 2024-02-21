7c56b32908dc254f52465a49770f005c2bb0ce8f
Ahmed Ihab <ahmedinouh@gmail.com>
2024-02-21 13:35:52 +0200

    nothing much

---
+++ javascript_app/app.js
@@ -0,0 +1,6 @@
+const inputfieldli = document.getElementById("input-field")
+const addbuttonel = document.getElementById("add-button")
+
+addbuttonel.addEventListener("click", function(){
+    console.log(inputfieldli.value)
+})
